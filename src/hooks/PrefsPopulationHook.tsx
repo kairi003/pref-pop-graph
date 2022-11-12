6475991f73e208e66de47e6c3dce9ba7ddf22793
kairi003 <kairi.satellite@gmail.com>
2022-11-13 05:33:14 +0900

    [update] データ構造等のリファクタリング

--- src/hooks/PrefsPopulationHook.tsx
+++ src/hooks/PrefsPopulationHook.tsx
@@ -3,19 +3,29 @@ import {getPrefectures, getPopulation} from 'utils/ResasClient';
 import {PrefectureData, TotalPopulationData} from 'utils/ResasModels';
 import {arrayUpdater} from 'utils/HookUtils';
 
+/**
+ * 人口グラフのSeriesと都道府県チェックを管理するカスタムフック
+ * @returns {series, prefProps}
+ */
 export const usePrefsPopulationSeries = () => {
   const [prefs, setPrefs] = useState<PrefectureData[]>([]);
   const [pops, setPops] = useState<TotalPopulationData[][]>([]);
   const [checkedList, setCheckedList] = useState<boolean[]>([]);
   const [loadingList, setLoadingList] = useState<boolean[]>([]);
 
-  const series = useMemo(() => pops.filter((_,i) => checkedList[i])
-    .map((popData, i) => ({
+  const series: ({type: 'line', name: string, data: {x: number, y:number}[]}[]) = useMemo(() => pops
+    .map((popData, i) => ({popData, ...prefs[i]}))
+    .filter((_,i) => checkedList[i])
+    .map(({popData, prefName, prefCode}, i) => ({
       type: 'line',
-      name: prefs[i],
-      data: popData
+      name: prefName,
+      data: popData.map(({year, value}) => ({x: year, y: value}))
     })), [pops, checkedList, prefs]);
 
+  /**
+   * マウント時に実行
+   * RESASから都道府県情報を得る
+   */
   useEffect(() => {
     getPrefectures().then(prefData => {
       setPrefs(prefData);
@@ -28,6 +38,10 @@ export const usePrefsPopulationSeries = () => {
     });
   }, []);
 
+  /**
+   * index番目の都道府県が選択されたとき
+   * @param index
+   */
   const checkHandler = useCallback((index: number): void => {
     setCheckedList(arrayUpdater(index, true));
     setLoadingList(arrayUpdater(index, true));
@@ -43,9 +57,13 @@ export const usePrefsPopulationSeries = () => {
     });
   }, [prefs]);
 
+  /**
+   * index番目の都道府県の選択が外れたとき
+   * @param index 
+   */
   const unCheckHander = useCallback((index: number): void => {
     setCheckedList(arrayUpdater(index, false));
   }, []);
 
-  return {series, prefs, checkedList, loadingList, checkHandler, unCheckHander};
+  return {series, prefProps: {prefs, checkedList, loadingList, onChecked: checkHandler, onUnChecked: unCheckHander}};
 }
