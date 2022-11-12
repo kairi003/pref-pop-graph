b0573cf8d5cc693c3edd5a1e7d41ddfe87c19457
kairi003 <kairi.satellite@gmail.com>
2022-11-13 05:29:40 +0900

    [update] コメント追加

--- src/utils/HookUtils.ts
+++ src/utils/HookUtils.ts
@@ -1,3 +1,9 @@
+/**
+ * setStateで `prev[i] = value` したいときのupdater関数を提供する
+ * @param index 更新するindex
+ * @param value 更新後のvalue
+ * @returns updater関数
+ */
 const arrayUpdater = <T,> (index: number, value: T): ((prev: T[]) => T[]) =>
   prev => prev.map((v,i) => (i === index) ? value : v);
 
