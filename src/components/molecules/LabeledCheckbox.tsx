a9f67a23f76d0329cef0253913425ea621023d2c
kairi003 <kairi.satellite@gmail.com>
2022-11-16 06:53:52 +0900

    [fix] チェックボックスリストアイテムのスタイルを修正
    - Typoで無効なクラスを修正
    - checkboxの状態をコンポーネント側で保持してクラスを決定するように変更

--- src/components/molecules/LabeledCheckbox.tsx
+++ src/components/molecules/LabeledCheckbox.tsx
@@ -1,14 +1,21 @@
 import Checkbox from 'components/atoms/Checkbox';
 import FormLabel from 'components/atoms/FormLabel';
+import { useState, useCallback } from 'react';
 import style from './LabeledCheckbox.module.css';
 
 type Props = React.ComponentPropsWithoutRef<'input'> & {
   labelText: string;
 };
-const LabeledCheckbox: React.FC<Props> = ({labelText, ...inputProps}) => {
-  return <div className={style.LabeledCheckbox}>
-    <Checkbox {...inputProps}/>
-    <FormLabel htmlFor={inputProps.id}>
+const LabeledCheckbox: React.FC<Props> = ({labelText, onChange, className, ...inputProps}) => {
+  const [checked, setChecked] = useState<boolean>(inputProps.checked === true);
+  const changeEventHandler: React.ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+    onChange?.(event);
+    setChecked(event.target.checked);
+  }, [onChange]);
+  const disabled = inputProps.disabled === true;
+  return <div className={[style.LabeledCheckbox, disabled&&style.disabled, checked&&style.checked, className].join(' ')}>
+    <Checkbox {...inputProps} onChange={changeEventHandler} className={[style.pointable].join(' ')}/>
+    <FormLabel htmlFor={inputProps.id} className={[style.pointable].join(' ')}>
       {labelText}
     </FormLabel>
   </div>
--- src/components/molecules/ToggleBlock.tsx
+++ src/components/molecules/ToggleBlock.tsx
@@ -1,14 +1,21 @@
 import Checkbox from 'components/atoms/Checkbox';
 import FormLabel from 'components/atoms/FormLabel';
+import { useState, useCallback } from 'react';
 import style from './ToggleBlock.module.css';
 
 type Props = React.ComponentPropsWithoutRef<'input'> & {
   labelText: string;
 };
-const ToggleBlock: React.FC<Props> = ({labelText, ...inputProps}) => {
-  return <div className={style.ToggleBlock}>
-    <Checkbox {...inputProps} className={style.Checkbox}/>
-    <FormLabel htmlFor={inputProps.id}  className={style.FormLabel}>
+const ToggleBlock: React.FC<Props> = ({labelText, onChange, className, ...inputProps}) => {
+  const [checked, setChecked] = useState<boolean>(inputProps.checked === true);
+  const changeEventHandler: React.ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+    onChange?.(event);
+    setChecked(event.target.checked);
+  }, [onChange]);
+  const disabled = inputProps.disabled === true;
+  return <div className={[style.ToggleBlock, disabled&&style.disabled, checked&&style.checked, className].join(' ')}>
+    <Checkbox {...inputProps} onChange={changeEventHandler} className={[style.Checkbox, style.pointable].join(' ')}/>
+    <FormLabel htmlFor={inputProps.id} className={[style.FormLabel, style.pointable].join(' ')}>
       {labelText}
     </FormLabel>
   </div>
