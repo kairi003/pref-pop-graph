f017806c0495f9b4a501b387bd997c685e8eafce
kairi003 <kairi.satellite@gmail.com>
2022-11-16 13:21:33 +0900

    [fix] チェックstateが正常に更新されないバグを修正
    stateで保持してたためpropsからの値の更新をスルーしてた

--- src/components/molecules/LabeledCheckbox.tsx
+++ src/components/molecules/LabeledCheckbox.tsx
@@ -7,12 +7,13 @@ type Props = React.ComponentPropsWithoutRef<'input'> & {
   labelText: string;
 };
 const LabeledCheckbox: React.FC<Props> = ({labelText, onChange, className, ...inputProps}) => {
-  const [checked, setChecked] = useState<boolean>(inputProps.checked === true);
+  const [checked, setChecked] = useState<boolean>(!!inputProps.checked);
+  if (!!inputProps.checked !== checked) setChecked(!!inputProps.checked);
   const changeEventHandler: React.ChangeEventHandler<HTMLInputElement> = useCallback(event => {
     onChange?.(event);
     setChecked(event.target.checked);
   }, [onChange]);
-  const disabled = inputProps.disabled === true;
+  const disabled = !!inputProps.disabled;
   return <div className={[style.LabeledCheckbox, disabled&&style.disabled, checked&&style.checked, className].join(' ')}>
     <Checkbox {...inputProps} onChange={changeEventHandler} className={style.pointable}/>
     <FormLabel htmlFor={inputProps.id} className={style.pointable}>
